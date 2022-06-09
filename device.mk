301541ffb406f3a4261b2c9c91dd6ff5b47b7597
Nolen Johnson <johnsonnolen@gmail.com>
2022-06-08 22:35:35 -0400

    deadpool: Inherit common tree after device vars are set
    * Allows several variables to be read in common tree.
    
    Change-Id: Ic8f3a4d210568254d814b013b67d8f06efff5e63

--- device.mk
+++ device.mk
@@ -8,8 +8,6 @@ DEVICE_PATH := device/askey/deadpool
 
 $(call inherit-product, vendor/askey/deadpool/deadpool-vendor.mk)
 
-$(call inherit-product, device/amlogic/g12-common/g12.mk)
-
 ## Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor
@@ -26,4 +24,8 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
+## Wi-Fi Firmware
 include hardware/amlogic/kernel-modules/dhd-driver/firmware/firmware.mk
+
+## Common Tree
+$(call inherit-product, device/amlogic/g12-common/g12.mk)
