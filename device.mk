d564fe3d35be7620da016d3f64540f7a16b5e83c
Bruno Martins <bgcngm@gmail.com>
2023-12-16 21:54:10 +0000

    deadpool: Properly build WLAN kernel module
    Change-Id: I1b02fd710c43ef66025d5d395728785c0ce3ddbe

--- device.mk
+++ device.mk
@@ -13,10 +13,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc
 
-## Kernel Modules
-PRODUCT_PACKAGES += \
-    dhd
-
 ## Keylayout (IR)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl
