4b5142de7acb386298401a2a451ebfa4f572c699
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-28 12:44:25 -0500

    deadpool: Support extracting from factory image
    Change-Id: I7bbafc75121403c460635b91b06e281590fda264

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.extract import extract_fns_user_type
+from extract_utils.extract_pixel import (
+    extract_pixel_factory_image,
+    extract_pixel_firmware,
+    pixel_factory_image_regex,
+    pixel_firmware_regex,
+)
+
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
@@ -27,12 +35,18 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('/vendor/lib/', '/vendor/lib/modules/')
 }  # fmt: skip
 
+extract_fns: extract_fns_user_type = {
+    pixel_factory_image_regex: extract_pixel_factory_image,
+    pixel_firmware_regex: extract_pixel_firmware,
+}
+
 module = ExtractUtilsModule(
     'deadpool',
     'askey',
     blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
     add_firmware_proprietary_file=True,
+    extract_fns=extract_fns,
 )
 
 if __name__ == '__main__':
